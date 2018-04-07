2a3e64e5a4802c94c79734a70fcf1122c2e0f0c5
Michał Warda <t1du53k@gmail.com>
2018-04-07 12:52:18 +0200

    Add simple done transaction

--- src/App.js
+++ src/App.js
@@ -2,30 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 
 const TransactionForm = () => {
-  return <form class="col-md-6 col-md-offset-3">
-  <h2>Spendings<br/><small>Put your spendings in the form</small></h2>
-       <div className="form-group">
-           <label htmlFor="absense">Amount</label>
-           <input className="form-control" name="amount" required="" type="number" /> ETH
-       </div>
-       <div className="form-group">
-           <label htmlFor="receiveaddress">Receive Address</label>
-           <input className="form-control" name="receiveaddress" required="" type="text" />
-       </div>
-       <div className="form-group">
-           <label htmlFor="description">Description</label>
-           <textarea className="form-control" name="description" required="" type="text" />
-       </div>
+  return (
+    <form class="col-md-6 col-md-offset-3">
+      <h2>
+        Spendings<br />
+        <small>Put your spendings in the form</small>
+      </h2>
+      <div className="form-group">
+        <label htmlFor="absense">Amount</label>
+        <input className="form-control" name="amount" required="" type="number" /> ETH
+      </div>
+      <div className="form-group">
+        <label htmlFor="receiveaddress">Receive Address</label>
+        <input className="form-control" name="receiveaddress" required="" type="text" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="description">Description</label>
+        <textarea className="form-control" name="description" required="" type="text" />
+      </div>
 
-   <div className="col-md-12">
-       <button id="requestSpending" type="button" className="btn btn-lg btn-primary">Request spending</button>
-   </div>
-  </form>
-}
+      <div className="col-md-12">
+        <button id="requestSpending" type="button" className="btn btn-lg btn-primary">
+          Request spending
+        </button>
+      </div>
+    </form>
+  );
+};
 
 const TransactionsList = () => {
-  return <div/>
-}
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-12">
+          <h1>Transactions</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <ul className="list-unstyled">
+            <li className="bv-transaction-item">
+              <p className="bv-transaction-item--amount">
+                <b>10 ETH</b>
+              </p>
+              <p>For a pc</p>
+              <p>0x8d12a197cb00d4747a1fe03395095c0000000000</p>
+              <div className="bv-transaction-item--status">✓</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
