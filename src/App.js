fb8d5aa8741bbd087307057db1ad9298ab1aecfa
Filip Noetzel <filip@j03.de>
2018-04-07 12:43:31 +0200

    Add minimal transaction form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 const TransactionForm = () => {
-  return <div/>
+  return <form class="col-md-6 col-md-offset-3">
+  <h2>Spendings<br/><small>Put your spendings in the form</small></h2>
+       <div className="form-group">
+           <label htmlFor="absense">Amount</label>
+           <input className="form-control" name="amount" required="" type="number" /> ETH
+       </div>
+       <div className="form-group">
+           <label htmlFor="receiveaddress">Receive Address</label>
+           <input className="form-control" name="receiveaddress" required="" type="text" />
+       </div>
+       <div className="form-group">
+           <label htmlFor="description">Description</label>
+           <textarea className="form-control" name="description" required="" type="text" />
+       </div>
+
+   <div className="col-md-12">
+       <button id="requestSpending" type="button" className="btn btn-lg btn-primary">Request spending</button>
+   </div>
+  </form>
 }
 
 const TransactionsList = () => {
